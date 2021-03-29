7fc4dee225ad0d68067e8114bc9cdea67a236050
Shayan <sh.shayan@gmail.com>
2021-03-29 21:59:37 +0430

    Basic Models and configs

--- accounting/admin.py
+++ accounting/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from . import models
+from .models import Profile
 
 # Register your models here.
-admin.site.register(models.Profile)
+admin.site.register(Profile)
